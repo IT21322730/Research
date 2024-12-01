638104c275cd8a9e0a41b13d775a79e0f90944ba
IT21319488 <99328158+IT21319488@users.noreply.github.com>
2024-12-01 09:04:41 +0530

    Update Tab.tsx

--- frontend/src/pages/all/Tabs.tsx
+++ frontend/src/pages/all/Tabs.tsx
@@ -14,6 +14,7 @@ import Pitta from './Pitta';
 import Kapha from './Kapha'
 
 import BlinkFinal from '../eye/BlinkFinal';
+import BlinkEye from '../eye/BlinkEye'
 
 import EyeHome from '../eye/EyeHome';
 import StepHome from './StepHome';
@@ -50,6 +51,7 @@ const Tabs: React.FC = () => {
             <Route path="/app/step" component={StepHome} />
 
             <Route path="/app/eyehome" component={EyeHome} />
+            <Route path="/app/blink" component={BlinkEye} />
             <Route path="/app/blinkfinal" component={BlinkFinal} />
 
             
