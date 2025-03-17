fa1f0ee4533a7b09a232a4ffc16af7278f862dfa
IT21322730 <99315565+IT21322730@users.noreply.github.com>
2025-03-17 23:14:08 +0530

    Tie detection related implementations

--- frontend/src/pages/all/Question.tsx
+++ frontend/src/pages/all/Question.tsx
@@ -45,7 +45,7 @@ const Question: React.FC = () => {
     }
   
     try {
-      const response = await fetch("http://localhost:5000/submit-questionnaire", {
+      const response = await fetch("http://127.0.0.1:5000/submit-questionnaire", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
